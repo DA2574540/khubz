d13e6af0c3c1207dfc02c3e548a035475c2b630b
DA2574540 <syafi.id.self@gmail.com>
2025-05-29 10:32:05 +0700

    d

--- script.js
+++ script.js
@@ -346,18 +346,3 @@ document.addEventListener('keydown', function(e) {
     closeModal();
   }
 });
-
-document.addEventListener('contextmenu', function(e){
-  e.preventDefault();
-});
-
-document.addEventListener('keydown', function(e){
-  if(
-    e.key === "F12"||
-    (e.ctrlKey && e.shiftKey && e.key === "I")||
-    (e.ctrlKey && e.key === "U")||
-    (e.ctrlKey && e.shiftKey && e.key === "j")
- ) {
-    e.preventDefault();
-  }
-});
